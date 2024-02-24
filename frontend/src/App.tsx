07515d5e800191e7364a2f42af93319629878033
Parbhat Sharma <prabhat29ps@gmail.com>
2024-02-24 22:08:07 +0530

    add redux for playlist

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/Homepage";
-import History from "./pages/History";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/history" element={<History />} />
       </Routes>
     </>
   );
